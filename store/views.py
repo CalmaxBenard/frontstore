82fb7a01b235170d0721edfe08ac140912eb7e72
CalmaxBenard <calmaxbenard@gmail.com>
2023-11-30 23:33:42 +0300

    initial commit

---
+++ store/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+from products.models import Category, Product
+
+def index(request):
+    products = Product.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+    return render(request, 'store/index.html', {
+        'categories': categories,
+        'products': products
+    })
+
+def contact(request):
+    return render(request, 'store/contact.html')
